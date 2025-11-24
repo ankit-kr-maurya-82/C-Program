590d0ba45d7d2ddcf03b6a5fb68d73454f75955c
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2025-11-24 21:47:39 +0530

    fix in array sumarray

--- sumaaray.c
+++ sumaaray.c
@@ -2,15 +2,34 @@
 void sumArr();
 
 int main() {
+    
     sumArr();
     return 0;
 }
 
-void sumArr(){
-    int sum=0;
-        int num[5]={10,20,30,40};
+// void sumArr(){
+//     int sum=0;
+//     int num[5]={10,20,30,40};
 
-    for (int i = 0; i < 5; i++){
+//     for (int i = 0; i < 5; i++){
+//         sum+=num[i];
+//     }
+//     printf("sum: %d",sum);
+    
+// }
+
+
+void sumArr(){
+    int sum=0, i;
+    int num[5];
+    printf("Enter 5 numbers: ");
+    for (i =0; i<5; i++){
+        printf("num[%d]:",i);
+        scanf("%d", &num[i]);
+    }
+    
+
+    for (i = 0; i < 5; i++){
         sum+=num[i];
     }
     printf("sum: %d",sum);
